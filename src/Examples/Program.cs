4a8abaef8015d8a1bc90b15f60dcd9c1fa790766
Folker Kinzel <fkinzel@freenet.de>
2023-07-23 20:05:12 +0200

    nuget package

--- src/Examples/Program.cs
+++ src/Examples/Program.cs
@@ -4,9 +4,9 @@ internal class Program
 {
     private static void Main()
     {
-        //FormattingOptionsExample.Example();
-        //FileExtensionExample.Example();
+        FormattingOptionsExample.Example();
+        FileExtensionExample.Example();
         BuildAndParseExample.Example();
-        //EqualityExample.Example();
+        EqualityExample.Example();
     }
 }
