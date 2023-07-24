6592a4eac9440c63fd3cd99a024a86ae785a8dc9
Folker Kinzel <fkinzel@freenet.de>
2023-07-25 01:21:29 +0200

    Examples

--- src/Examples/BuildAndParseExample.cs
+++ src/Examples/BuildAndParseExample.cs
@@ -7,7 +7,7 @@ public static class BuildAndParseExample
     public static void Example()
     {
         const string longParameterValue = """
-        This is a very long parameter that will be wrapped according to RFC 2184.
+        This is a very long parameter that will be wrapped according to RFC 2231.
         It also contains a few Non-ASCII-Characters: äöß.
         """;
 
@@ -45,7 +45,7 @@ Console Output:
 application/x-stuff;
 first-parameter*0*=utf-8'en'This%20is%20a%20very%20long%20param;
 first-parameter*1*=eter%20that%20will%20be%20wrapped%20accordin;
-first-parameter*2*=g%20to%20RFC%202184.%0D%0AIt%20also%20contai;
+first-parameter*2*=g%20to%20RFC%202231.%0D%0AIt%20also%20contai;
 first-parameter*3*=ns%20a%20few%20Non-ASCII-Characters%3A%20%C3;
 first-parameter*4*=%A4%C3%B6%C3%9F.;
 second-parameter="Parameter with  \\, = and \"."
@@ -58,7 +58,7 @@ Parameter 1:
 Key:      first-parameter
 Language: en
 Value:
-This is a very long parameter that will be wrapped according to RFC 2184.
+This is a very long parameter that will be wrapped according to RFC 2231.
 It also contains a few Non-ASCII-Characters: äöß.
 
 Parameter 2:
--- src/Examples/Program.cs
+++ src/Examples/Program.cs
@@ -4,12 +4,12 @@ internal class Program
 {
     private static void Main()
     {
-        FormattingOptionsExample.Example();
-        Console.WriteLine();
-        FileExtensionExample.Example();
-        Console.WriteLine();
+        //FormattingOptionsExample.Example();
+        //Console.WriteLine();
+        //FileExtensionExample.Example();
+        //Console.WriteLine();
         BuildAndParseExample.Example();
-        Console.WriteLine();
-        EqualityExample.Example();
+        //Console.WriteLine();
+        //EqualityExample.Example();
     }
 }
