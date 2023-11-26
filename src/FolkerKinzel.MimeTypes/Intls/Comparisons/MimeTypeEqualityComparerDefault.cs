8128373c81f22351a990dd10f7538ab7d82d6398
Folker Kinzel <fkinzel@freenet.de>
2023-11-26 14:48:50 +0100

    .NET 8

--- src/FolkerKinzel.MimeTypes/Intls/Comparisons/MimeTypeEqualityComparerDefault.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FolkerKinzel.MimeTypes.Intls.Comparisons;
-
-internal sealed class MimeTypeEqualityComparerDefault : MimeTypeInfoEqualityComparer
-{
-    public override bool Equals(MimeTypeInfo x, MimeTypeInfo y) => x.Equals(y);
-
-    public override int GetHashCode([DisallowNull] MimeTypeInfo obj) => obj.GetHashCode();
-}
--- src/FolkerKinzel.MimeTypes/Intls/Comparisons/MimeTypeEqualityComparerIgnoreParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FolkerKinzel.MimeTypes.Intls.Comparisons;
-
-internal sealed class MimeTypeEqualityComparerIgnoreParameters : MimeTypeInfoEqualityComparer
-{
-    public override bool Equals(MimeTypeInfo x, MimeTypeInfo y) => x.Equals(y, true);
-
-    public override int GetHashCode([DisallowNull] MimeTypeInfo obj) => obj.GetHashCode(true);
-}
